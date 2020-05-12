2596c52c938d5b2c95a4815dfe8eacab427bd27a
EmmanuelAyTuz <tuzemmanuel@gmail.com>
2020-05-12 16:52:59 -0500

    taget blank

--- auto.js
+++ auto.js
@@ -5,7 +5,7 @@ $(document).ready(() => {
       $("#listexe").append(
         "<a href='" +
           data[i].url +
-          "' class='list-group-item list-group-item-action flex-column align-items-start'><div class='d-flex w-100 justify-content-between'><h5 class='mb-1'><i class='fa fa-home'></i> " +
+          "' target='_blank' class='list-group-item list-group-item-action flex-column align-items-start'><div class='d-flex w-100 justify-content-between'><h5 class='mb-1'><i class='fa fa-home'></i> " +
           data[i].title +
           "</h5><span class='badge badge-primary badge-pill'>" +
           (i + 1) +
